bb1be7bdb09c8315d09c339525aa0aec095edb9d
federico-ramirez <00009220@uca.edu.sv>
2021-06-24 22:54:08 -0600

    Arreglando imagen

--- Proyecto-POO/Proyecto-POO/View/Form1.Designer.cs
+++ Proyecto-POO/Proyecto-POO/View/Form1.Designer.cs
@@ -37,8 +37,8 @@ namespace Proyecto_POO
             this.btnLogin = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblForgotPassword = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -112,21 +112,11 @@ namespace Proyecto_POO
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Sans", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(158, 42);
+            this.label3.Location = new System.Drawing.Point(148, 37);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(399, 55);
             this.label3.TabIndex = 1;
             this.label3.Text = "BIENVENIDO/A";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(36, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(174, 122);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // lblForgotPassword
             // 
@@ -139,6 +129,15 @@ namespace Proyecto_POO
             this.lblForgotPassword.Text = "¿Has olvidado tu contraseña?";
             this.lblForgotPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblForgotPassword_LinkClicked);
             this.lblForgotPassword.Click += new System.EventHandler(this.lblForgotPassword_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -179,8 +178,8 @@ namespace Proyecto_POO
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.LinkLabel lblForgotPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
